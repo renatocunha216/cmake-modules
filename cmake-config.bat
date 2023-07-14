d03d8ba6222b058632f176a8a4b691037947c45e
rbcunha79 <rbcunha@yahoo.com>
2023-07-14 10:55:30 -0300

    using the MINGW_HOME environment variable

--- cmake-config.bat
+++ cmake-config.bat
@@ -13,7 +13,7 @@ cd _debug
 rem Generate make configuration files for MinGW in DEBUG mode (cmake default)
 call cmake ^
 -D CMAKE_BUILD_TYPE=Debug ^
--D "CMAKE_MAKE_PROGRAM:PATH=C:/mingw64/bin/make.exe" ^
+-D "CMAKE_MAKE_PROGRAM:PATH=%MINGW_HOME%/bin/make.exe" ^
 -G "MinGW Makefiles" ..
 cd ..
 cd _release
@@ -21,7 +21,7 @@ cd _release
 rem Generate make configuration files for MinGW in RELEASE mode (cmake default)
 call cmake ^
 -D CMAKE_BUILD_TYPE=Release ^
--D "CMAKE_MAKE_PROGRAM:PATH=C:/mingw64/bin/make.exe" ^
+-D "CMAKE_MAKE_PROGRAM:PATH=%MINGW_HOME%/bin/make.exe" ^
 -G "MinGW Makefiles" ..
 rem cd ..
 rem cd _test
@@ -29,7 +29,7 @@ rem cd _test
 rem Generate make configuration files for MinGW in TEST mode (custom)
 rem call cmake ^
 rem -D CMAKE_BUILD_TYPE=Test ^
-rem -D "CMAKE_MAKE_PROGRAM:PATH=C:/mingw64/bin/make.exe" ^
+rem -D "CMAKE_MAKE_PROGRAM:PATH=%MINGW_HOME%/bin/make.exe" ^
 rem -G "MinGW Makefiles" ..
 rem cd ..
 @echo End of creation of build files configuration using CMAKE.
